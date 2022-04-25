e084183ea7bc2a5aa4a033075238555595c5a2fd
brennop <brennopcordeiro@gmail.com>
2022-04-24 23:02:06 -0300

    add pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), VitePWA({
+    manifest: {
+      name: "Unrel",
+      short_name: "Unrel",
+      description: "Music Player",
+      theme_color: "#ffffff",
+    }
+  })]
 })
