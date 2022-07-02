6db7c5975500e3e001a7d48750e9d13b76fb74c1
brennop <brennopcordeiro@gmail.com>
2022-07-02 13:33:18 -0300

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA()],
   resolve: {
     alias: {
       'components': path.resolve(__dirname, './src/components'),
